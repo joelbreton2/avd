8cca11a298651f5e31b00b8aa57ffe446337b1ef
Shivani-gslab <145646625+Shivani-gslab@users.noreply.github.com>
2025-01-31 20:05:40 +0530

    Refactor(eos_designs): Refactor eos_designs structured_config code for tunnel_interfaces (#4946)

--- python-avd/pyavd/_eos_designs/structured_config/network_services/tunnel_interfaces.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/tunnel_interfaces.py
@@ -3,10 +3,10 @@
 # that can be found in the LICENSE file.
 from __future__ import annotations
 
-from functools import cached_property
 from typing import TYPE_CHECKING, Protocol
 
-from pyavd._utils import append_if_not_duplicate
+from pyavd._eos_cli_config_gen.schema import EosCliConfigGen
+from pyavd._eos_designs.structured_config.structured_config_generator import structured_config_contributor
 
 if TYPE_CHECKING:
     from . import AvdStructuredConfigNetworkServicesProtocol
@@ -19,44 +19,31 @@ class TunnelInterfacesMixin(Protocol):
     Class should only be used as Mixin to a AvdStructuredConfig class.
     """
 
-    @cached_property
-    def tunnel_interfaces(self: AvdStructuredConfigNetworkServicesProtocol) -> list | None:
+    @structured_config_contributor
+    def tunnel_interfaces(self: AvdStructuredConfigNetworkServicesProtocol) -> None:
         """
-        Return structured config for tunnel_interfaces.
+        Set structured config for tunnel_interfaces.
 
         Only used for CV Pathfinder edge routers today
         """
         if not self._filtered_internet_exit_policies_and_connections:
-            return None
-
-        tunnel_interfaces = []
+            return
 
         for internet_exit_policy, connections in self._filtered_internet_exit_policies_and_connections:
             for connection in connections:
                 if connection["type"] == "tunnel":
-                    tunnel_interface = {
-                        "name": f"Tunnel{connection['tunnel_id']}",
-                        "description": connection["description"],
-                        "mtu": 1394,  # TODO: do not hardcode
-                        "ip_address": connection["tunnel_ip_address"],
-                        "tunnel_mode": "ipsec",  # TODO: do not hardcode
-                        "source_interface": connection["source_interface"],
-                        "destination": connection["tunnel_destination_ip"],
-                        "ipsec_profile": connection["ipsec_profile"],
-                    }
-
-                    if internet_exit_policy.type == "zscaler":
-                        tunnel_interface["nat_profile"] = self.get_internet_exit_nat_profile_name(internet_exit_policy.type)
-
-                    append_if_not_duplicate(
-                        list_of_dicts=tunnel_interfaces,
-                        primary_key="name",
-                        new_dict=tunnel_interface,
-                        context="Tunnel interface for Internet Exit policy",
-                        context_keys=["name"],
+                    tunnel_interface = EosCliConfigGen.TunnelInterfacesItem(
+                        name=f"Tunnel{connection['tunnel_id']}",
+                        description=connection["description"],
+                        mtu=1394,  # TODO: do not hardcode
+                        ip_address=connection["tunnel_ip_address"],
+                        tunnel_mode="ipsec",  # TODO: do not hardcode
+                        source_interface=connection["source_interface"],
+                        destination=connection["tunnel_destination_ip"],
+                        ipsec_profile=connection["ipsec_profile"],
                     )
 
-        if tunnel_interfaces:
-            return tunnel_interfaces
+                    if internet_exit_policy.type == "zscaler":
+                        tunnel_interface.nat_profile = self.get_internet_exit_nat_profile_name(internet_exit_policy.type)
 
-        return None
+                    self.structured_config.tunnel_interfaces.append(tunnel_interface)
