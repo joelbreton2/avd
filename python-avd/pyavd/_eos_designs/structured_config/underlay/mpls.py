e1c6985b5a42f6d05ac3e10cc97468b97d70db1b
Vibhu-gslab <109593615+Vibhu-gslab@users.noreply.github.com>
2025-02-04 16:12:38 +0530

    Refactor(eos_designs): structured_config for underlay mpls (#4963)
    Co-authored-by: Mahesh Kumar <122076792+MaheshGSLAB@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- python-avd/pyavd/_eos_designs/structured_config/underlay/mpls.py
+++ python-avd/pyavd/_eos_designs/structured_config/underlay/mpls.py
@@ -3,10 +3,9 @@
 # that can be found in the LICENSE file.
 from __future__ import annotations
 
-from functools import cached_property
 from typing import TYPE_CHECKING, Protocol
 
-from pyavd._utils import strip_empties_from_dict
+from pyavd._eos_designs.structured_config.structured_config_generator import structured_config_contributor
 
 if TYPE_CHECKING:
     from . import AvdStructuredConfigUnderlayProtocol
@@ -19,23 +18,17 @@ class MplsMixin(Protocol):
     Class should only be used as Mixin to a AvdStructuredConfig class.
     """
 
-    @cached_property
-    def mpls(self: AvdStructuredConfigUnderlayProtocol) -> dict | None:
+    @structured_config_contributor
+    def mpls(self: AvdStructuredConfigUnderlayProtocol) -> None:
         """Return structured config for mpls."""
         if self.shared_utils.underlay_mpls is not True:
-            return None
+            return
 
+        self.structured_config.mpls.ip = True
         if self.shared_utils.underlay_ldp is True:
-            return strip_empties_from_dict(
-                {
-                    "ip": True,
-                    "ldp": {
-                        "interface_disabled_default": True,
-                        "router_id": self.shared_utils.router_id if not self.inputs.use_router_general_for_router_id else None,
-                        "shutdown": False,
-                        "transport_address_interface": "Loopback0",
-                    },
-                }
+            self.structured_config.mpls.ldp._update(
+                interface_disabled_default=True,
+                router_id=self.shared_utils.router_id if not self.inputs.use_router_general_for_router_id else None,
+                shutdown=False,
+                transport_address_interface="Loopback0",
             )
-
-        return {"ip": True}
