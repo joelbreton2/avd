35c78a1c8432d15472e1c48415098867dd6754fc
Mahesh Kumar <122076792+MaheshGSLAB@users.noreply.github.com>
2025-02-05 15:28:41 +0530

    Refactor(eos_designs): Refactor eos_designs structured_config code for virtual_source_nat_vrfs (#4974)
    Co-authored-by: Mahesh Kumar <mahesh.kumar-ext@Maheshs-MacBook-Pro.local>
    Co-authored-by: Guillaume Mulocher <gmulocher@arista.com>

--- python-avd/pyavd/_eos_designs/structured_config/network_services/virtual_source_nat_vrfs.py
+++ python-avd/pyavd/_eos_designs/structured_config/network_services/virtual_source_nat_vrfs.py
@@ -3,10 +3,11 @@
 # that can be found in the LICENSE file.
 from __future__ import annotations
 
-from functools import cached_property
 from typing import TYPE_CHECKING, Protocol
 
-from pyavd._utils import append_if_not_duplicate, get_ip_from_ip_prefix, strip_null_from_data
+from pyavd._eos_cli_config_gen.schema import EosCliConfigGen
+from pyavd._eos_designs.structured_config.structured_config_generator import structured_config_contributor
+from pyavd._utils import get_ip_from_ip_prefix
 
 if TYPE_CHECKING:
     from . import AvdStructuredConfigNetworkServicesProtocol
@@ -19,43 +20,32 @@ class VirtualSourceNatVrfsMixin(Protocol):
     Class should only be used as Mixin to a AvdStructuredConfig class.
     """
 
-    @cached_property
-    def virtual_source_nat_vrfs(self: AvdStructuredConfigNetworkServicesProtocol) -> list | None:
+    @structured_config_contributor
+    def virtual_source_nat_vrfs(self: AvdStructuredConfigNetworkServicesProtocol) -> None:
         """
-        Return structured config for virtual_source_nat_vrfs.
+        Set the structured config for virtual_source_nat_vrfs.
 
         Only used by VTEPs with L2 and L3 services
         Using data from loopback_interfaces to avoid duplicating logic
         """
         if not (self.shared_utils.overlay_vtep and self.shared_utils.network_services_l2 and self.shared_utils.network_services_l3):
-            return None
+            return
 
         if (loopback_interfaces := self.loopback_interfaces) is None:
-            return None
+            return
 
-        virtual_source_nat_vrfs = []
         for loopback_interface in loopback_interfaces:
             if (vrf := loopback_interface.get("vrf", "default")) is None:
                 continue
 
-            # Using append_if_not_duplicate but with ignore_same_dict and ignore_keys.
-            # It will append the dict unless the same "name" is already in the dict.
+            # Using append with ignore_fields.
+            # It will append the VirtualSourceNatVrfsItem unless the same "name" is already in the list.
             # It will never raise since we only have these two keys.
-            append_if_not_duplicate(
-                virtual_source_nat_vrfs,
-                primary_key="name",
-                new_dict={
-                    "name": vrf,
-                    "ip_address": get_ip_from_ip_prefix(loopback_interface["ip_address"]) if "ip_address" in loopback_interface else None,
-                    "ipv6_address": get_ip_from_ip_prefix(loopback_interface["ipv6_address"]) if "ipv6_address" in loopback_interface else None,
-                },
-                context="virtual_source_nat_vrfs",
-                context_keys=["name"],
-                ignore_same_dict=True,
-                ignore_keys={"ip_address", "ipv6_address"},
+            self.structured_config.virtual_source_nat_vrfs.append(
+                EosCliConfigGen.VirtualSourceNatVrfsItem(
+                    name=vrf,
+                    ip_address=get_ip_from_ip_prefix(loopback_interface["ip_address"]) if "ip_address" in loopback_interface else None,
+                    ipv6_address=get_ip_from_ip_prefix(loopback_interface["ipv6_address"]) if "ipv6_address" in loopback_interface else None,
+                ),
+                ignore_fields=("ip_address", "ipv6_address"),
             )
-
-        if virtual_source_nat_vrfs:
-            return strip_null_from_data(virtual_source_nat_vrfs)
-
-        return None
