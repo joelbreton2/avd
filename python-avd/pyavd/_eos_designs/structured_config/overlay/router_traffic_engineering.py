c81ac16284266fefbf9543b78f4a508e8d5fa761
Shivani-gslab <145646625+Shivani-gslab@users.noreply.github.com>
2025-02-10 14:26:17 +0530

    Refactor(eos_designs): Refactor eos_designs structured_config code for overlay/router_traffic_engineering (#4992)

--- python-avd/pyavd/_eos_designs/structured_config/overlay/router_traffic_engineering.py
+++ python-avd/pyavd/_eos_designs/structured_config/overlay/router_traffic_engineering.py
@@ -3,8 +3,9 @@
 # that can be found in the LICENSE file.
 from __future__ import annotations
 
-from functools import cached_property
 from typing import TYPE_CHECKING, Protocol
+
+from pyavd._eos_designs.structured_config.structured_config_generator import structured_config_contributor
 
 if TYPE_CHECKING:
     from . import AvdStructuredConfigOverlayProtocol
@@ -17,10 +18,10 @@ class RouterTrafficEngineering(Protocol):
     Class should only be used as Mixin to a AvdStructuredConfig class.
     """
 
-    @cached_property
-    def router_traffic_engineering(self: AvdStructuredConfigOverlayProtocol) -> dict | None:
-        """Return structured config for router traffic-engineering."""
+    @structured_config_contributor
+    def router_traffic_engineering(self: AvdStructuredConfigOverlayProtocol) -> None:
+        """Set the structured config for router traffic-engineering."""
         if not self.shared_utils.is_cv_pathfinder_router:
-            return None
+            return
 
-        return {"enabled": True}
+        self.structured_config.router_traffic_engineering.enabled = True
